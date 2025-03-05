77a89597643a405e4266317ee9e463d049486fe8
Conner LeBlanc <col397@usask.ca>
2025-03-05 11:04:43 -0600

    Modified the grid300 page to display the component gridlist300

--- src/pages/Grid300.jsx
+++ src/pages/Grid300.jsx
@@ -1,8 +1,14 @@
+import GridList300 from '../components/grid/GridList300';
 const Grid300 = () => {
   return (
     <div className="bg-blue-500 font-bold text-4xl text-white text-center p-4">
       Grid 300
+
+        <h1>
+            <GridList300></GridList300>
+        </h1>
     </div>
+
   );
 };
 
