da2c6f07d629da3b995ec665ab93e55a89330e7d
Conner LeBlanc <col397@usask.ca>
2025-03-05 17:49:41 -0600

    updated the grid 300 file to have a header and text below for where the fonts will be

--- src/pages/Grid300.jsx
+++ src/pages/Grid300.jsx
@@ -1,14 +1,15 @@
 import GridList300 from '../components/grid/GridList300';
 const Grid300 = () => {
   return (
-    <div className="bg-blue-500 font-bold text-4xl text-white text-center p-4">
-      Grid 300
+    <div>
+        <header className="bg-blue-500 font-bold text-4xl text-white text-center p-4">
+            Grid 300
+        </header>
 
-        <h1>
+        <text>
             <GridList300></GridList300>
-        </h1>
+        </text>
     </div>
-
   );
 };
 
