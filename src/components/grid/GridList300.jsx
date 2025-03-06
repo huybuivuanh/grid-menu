6269f27ff0b5adae0c535e11abf8524132baac0a
Conner LeBlanc <col397@usask.ca>
2025-03-05 22:03:56 -0600

    Added a function to change the button color to blue when selected and grey otherwise

--- src/components/grid/GridList300.jsx
+++ src/components/grid/GridList300.jsx
@@ -6,6 +6,9 @@ const fonts = await fetchFonts(300); // Use fetchFonts to get the 300 font
 
 //let fontText = [] // an array containing the names of all the fonts
 
+let selected = 0; // start the selected icon at 0 for the font
+let prev = "";
+
 
 export default function GridList300(){
     // load the fonts
@@ -14,44 +17,40 @@ export default function GridList300(){
     }
     return(
     <div>
-
-        <table>
-            <tbody>
+        <table align={"center"}>
+            <tbody align={"center"}>
             <tr>
-                <td id={"testID"}>
-                    <button id="1" style={{fontFamily: fonts[0].family, background: "lightgrey" }} > Font 1</button>
-                    <button id="2" style={{fontFamily: fonts[1].family, background: "lightgrey"  }} > Font 2</button>
-                    <button id="3" style={{fontFamily: fonts[2].family, background: "lightgrey"  }} > Font 3</button>
-                    <button id="4" style={{fontFamily: fonts[3].family, background: "lightgrey"  }} > Font 4</button>
-                    <button id="5" style={{fontFamily: fonts[4].family, background: "lightgrey"  }} > Font 5</button>
-                    <button id="6" style={{fontFamily: fonts[5].family, background: "lightgrey"  }} > Font 6</button>
-                    <button id="7" style={{fontFamily: fonts[6].family, background: "lightgrey"  }} > Font 7</button>
-                    <button id="8" style={{fontFamily: fonts[7].family, background: "lightgrey"  }} > Font 8</button>
-                    <button id="9" style={{fontFamily: fonts[8].family, background: "lightgrey"  }} > Font 9</button>
-                    <button id="10" style={{fontFamily: fonts[9].family, background: "lightgrey"  }} > Font 10</button>
-                    <button id="11" style={{fontFamily: fonts[10].family , background: "lightgrey" }} > Font 11</button>
-                    <button id="12" style={{fontFamily: fonts[11].family, background: "lightgrey"  }} > Font 12</button>
-                    <button id="13" style={{fontFamily: fonts[12].family , background: "lightgrey" }} > Font 13</button>
-                    <button id="14" style={{fontFamily: fonts[13].family , background: "lightgrey" }} > Font 14</button>
-                    <button id="15" style={{fontFamily: fonts[14].family , background: "lightgrey" }} > Font 15</button>
-                    <button id="16" style={{fontFamily: fonts[15].family , background: "lightgrey" }} > Font 16</button>
-                    <button id="17" style={{fontFamily: fonts[16].family , background: "lightgrey" }} > Font 17</button>
-                    <button id="18" style={{fontFamily: fonts[17].family , background: "lightgrey" }} > Font 18</button>
-                    <button id="19" style={{fontFamily: fonts[18].family , background: "lightgrey" }} > Font 19</button>
-                    <button id="20" style={{fontFamily: fonts[19].family , background: "lightgrey" }} > Font 20</button>
+                <td id={"row1"} align={"center"}>
+                    <button id="1" style={{fontFamily: fonts[0].family, background: "lightgrey", fontSize: 25, padding: "10px 10px" }} onClick={ () => clicked("1")} > Font 1</button>
+                    <button id="2" style={{fontFamily: fonts[1].family, background: "lightgrey", fontSize: 25, padding: "10px 10px"   }} onClick={ () => clicked("2")}   > Font 2</button>
+                    <button id="3" style={{fontFamily: fonts[2].family, background: "lightgrey", fontSize: 25, padding: "10px 10px"   }} onClick={ () => clicked("3")} > Font 3</button>
+                    <button id="4" style={{fontFamily: fonts[3].family, background: "lightgrey", fontSize: 25, padding: "10px 10px"   }} onClick={ () => clicked("4")} > Font 4</button>
+                    <button id="5" style={{fontFamily: fonts[4].family, background: "lightgrey", fontSize: 25, padding: "10px 10px"   }} onClick={ () => clicked("5")} > Font 5</button>
                 </td>
             </tr>
             <tr>
                 <td id="testID2">
                     test 1
-                    test 2
-                    test new row
                 </td>
             </tr>
             </tbody>
 
         </table>
-    <TextBox selectedFont={fonts[5]}></TextBox>
+    <TextBox id ="tb" selectedFont={fonts[selected].family}></TextBox>
     </div>
     )
+}
+
+function clicked(id){
+    if (prev === ""){ // if no previous selected font
+        prev = id; // set the new prev to the current font
+    }
+    else{ // else reset the prev button color
+        document.getElementById(prev).style.background = "lightgrey";
+        prev = id;
+    }
+
+    selected = id;
+    document.getElementById(id).style.background = "royalblue";
+    document.getElementById(id).style.color = "white";
 }
