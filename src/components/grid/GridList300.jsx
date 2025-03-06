270dbf97043a74e2be74bb7408eda15f82d437c2
Conner LeBlanc <col397@usask.ca>
2025-03-06 02:22:13 -0600

    Created a function to create buttons, and created 300 buttons and added them to the screen

--- src/components/grid/GridList300.jsx
+++ src/components/grid/GridList300.jsx
@@ -2,41 +2,26 @@ import {fetchFonts, loadGoogleFont} from "../FontLoading.js";
 import TextBox from "../TextArea.jsx";
 
 const fonts = await fetchFonts(300); // Use fetchFonts to get the 300 font
-
-
-//let fontText = [] // an array containing the names of all the fonts
-
 let selected = 0; // start the selected icon at 0 for the font
 let prev = "";
+let buttonArray = [];
 
+// create a new button with the next font
+function createButton(buttonNum){
+    console.log(buttonNum);
+    return  <button id={buttonNum + ""} style={{fontFamily: fonts[buttonNum].family, background: "lightgrey", fontSize: 10, padding: "10px 10px", margin: "5px 5px", width: "10%"}} onClick={ () => clicked(buttonNum)} > {fonts[buttonNum].family}</button>;
+}
 
 export default function GridList300(){
     // load the fonts
     for (let i = 0; i < 300; i++) {
         loadGoogleFont(fonts[i].family);
+        buttonArray[i] = createButton(i); // create 300 buttons for the table
     }
     return(
-    <div>
-        <table align={"center"}>
-            <tbody align={"center"}>
-            <tr>
-                <td id={"row1"} align={"center"}>
-                    <button id="1" style={{fontFamily: fonts[0].family, background: "lightgrey", fontSize: 25, padding: "10px 10px" }} onClick={ () => clicked("1")} > Font 1</button>
-                    <button id="2" style={{fontFamily: fonts[1].family, background: "lightgrey", fontSize: 25, padding: "10px 10px"   }} onClick={ () => clicked("2")}   > Font 2</button>
-                    <button id="3" style={{fontFamily: fonts[2].family, background: "lightgrey", fontSize: 25, padding: "10px 10px"   }} onClick={ () => clicked("3")} > Font 3</button>
-                    <button id="4" style={{fontFamily: fonts[3].family, background: "lightgrey", fontSize: 25, padding: "10px 10px"   }} onClick={ () => clicked("4")} > Font 4</button>
-                    <button id="5" style={{fontFamily: fonts[4].family, background: "lightgrey", fontSize: 25, padding: "10px 10px"   }} onClick={ () => clicked("5")} > Font 5</button>
-                </td>
-            </tr>
-            <tr>
-                <td id="testID2">
-                    test 1
-                </td>
-            </tr>
-            </tbody>
-
-        </table>
-    <TextBox id ="tb" selectedFont={fonts[selected].family}></TextBox>
+    <div id="d1" align="center">
+        {buttonArray}
+    <TextBox selectedFont={fonts[selected]}></TextBox>
     </div>
     )
 }
@@ -47,6 +32,7 @@ function clicked(id){
     }
     else{ // else reset the prev button color
         document.getElementById(prev).style.background = "lightgrey";
+        document.getElementById(prev).style.color = "black";
         prev = id;
     }
 
