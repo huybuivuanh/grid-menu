ebbba8a228feb47090ed371fbcaaea82d8a95f95
Conner LeBlanc <col397@usask.ca>
2025-03-06 20:43:40 -0600

    Added some comments and polished up some code

--- src/components/grid/GridList300.jsx
+++ src/components/grid/GridList300.jsx
@@ -3,25 +3,23 @@ import TextBox from "../TextArea.jsx";
 import {useState} from "react";
 
 const fonts = await fetchFonts(300); // Use fetchFonts to get the 300 font
-let selected = 0; // start the selected icon at 0 for the font
 let prev = ""; // store the prev coloured buttons id to change its colour back
 const buttonArray = []; // create an array to store all the buttons created
 
 export default function GridList300(){
-    for (let i = 0; i < 300; i++) {
+    for (let i = 0; i < 300; i++) { // for 300 fonts
         loadGoogleFont(fonts[i].family); // load the fonts
         buttonArray[i] = createButton(i); // create 300 buttons for the table
     }
-
     // create a use state to update the text box each button press
     const[currID, setCurrID] = useState(0);
 
     // create a new button with the next font
     function createButton(buttonNum){
-        console.log(buttonNum);
+        // create a button which displays a new font option
         return  <button
             id={buttonNum + ""}
-            style={{fontFamily: fonts[buttonNum].family, background: "lightgrey", fontSize: 10, padding: "10px 10px", margin: "5px 5px", width: "15%"}}
+            style={{fontFamily: fonts[buttonNum].family, background: "lightgrey", fontSize: 10, padding: "5px 5px", margin: "5px 5px", width: "15%"}}
             onClick={ () => clicked(buttonNum)} >
             {fonts[buttonNum].family}
         </button>;
@@ -37,15 +35,15 @@ export default function GridList300(){
             prev = id;
         }
 
-        selected = id; // set the selected button as the id
+        // set the button font and background colour
         document.getElementById(id).style.background = "royalblue";
         document.getElementById(id).style.color = "white";
-        setCurrID(selected);
+        setCurrID(id); // set the selected ID
     }
     return(
     <div id="d1" align="center">
         {buttonArray}
-        <TextBox id="tb" selectedFont={fonts[currID].family}></TextBox>
+        <TextBox selectedFont={fonts[currID].family}></TextBox>
     </div>
     )
 }
