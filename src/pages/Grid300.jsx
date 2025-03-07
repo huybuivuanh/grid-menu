53e75d046ab23177d37daae3ca975e5123b12deb
Conner LeBlanc <col397@usask.ca>
2025-03-07 15:31:45 -0600

    Changed header size, and buttons sizes so they all fit on the screen

--- src/pages/Grid300.jsx
+++ src/pages/Grid300.jsx
@@ -2,7 +2,7 @@ import GridList300 from '../components/grid/GridList300';
 const Grid300 = () => {
   return (
     <div>
-        <header className="bg-blue-500 font-bold text-4xl text-white text-center p-4">
+        <header className="bg-blue-500 font-bold text-1xl text-white text-center p-1">
             Grid 300
         </header>
 
