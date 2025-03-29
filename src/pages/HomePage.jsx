8052e613c4cd7a40627f8ddb4894f235d919975e
Huy Bui (wgv256) <160438131+huybuivuanh@users.noreply.github.com>
2025-03-29 14:56:38 -0600

    Fix import path for Homepage again

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,6 @@
 import HomepageDescription from "../components/HomepageDescription";
 
-const Homepage = () => {
+const HomePage = () => {
   return (
     <div>
       <HomepageDescription></HomepageDescription>
@@ -8,4 +8,4 @@ const Homepage = () => {
   );
 };
 
-export default Homepage;
+export default HomePage;
